d6e37567fb854556b9be268f073d428d227f64ba
rmaus8 <rmausolf06@gmail.com>
2018-08-23 15:36:48 -0500

    revamp website

---
+++ js/flipbook.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	let count = 0
+	const screenSize = window.matchMedia("(orientation: portrait)");
+
+	$(window).ready(function() {
+		checkScreen(screenSize);
+		screenSize.addListener(checkScreen);
+	});	
+
+	function checkScreen(screenSize) {
+		if (screenSize.matches) {
+			$("#flipbook").turn({
+				display: 'single'
+			});
+		} else {
+			$("#flipbook").turn({ 
+				display: 'double',
+				autoCenter: false
+			});
+		}
+	}
+
+	$("#flipbook").bind("turning", function(event, page, view) {
+		count = page
+		$('#number').html(count);		
+		if (page == 2) {
+			$("#flipbook").css("margin", "0 auto")
+		} else if (page == 1) {
+			$("#flipbook").css("margin-left", "-38vw")
+		} else if (page == 22) {
+			$("#flipbook").css("margin-right", "-38vw");
+		} else if (page == 21) {
+			$("#flipbook").css("margin", "0 auto")
+		}
+	})
+
+	$('#flipbook').change(function(){
+		alert("changed")
+		$('#number').html(count);
+	})
+
+	window.onorientationchange = function() { 
+		window.location.reload(); 
+	};
+
+	window.onresize = function(){ location.reload();  }
+
+	$(window).bind('keydown', function(e){
+	    if (e.keyCode==37)
+	        $('#flipbook').turn('previous');
+	    else if (e.keyCode==39)
+	        $('#flipbook').turn('next');
+	});
+
+	$(".right").click(function(){
+		$('#flipbook').turn('next');
+	});
+	$(".left").click(function(){
+		$('#flipbook').turn('previous');
+	});
+
+});

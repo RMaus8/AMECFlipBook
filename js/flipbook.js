a465b5c268a3c07850268fc87c4275a530736caa
rmaus8 <rmausolf06@gmail.com>
2018-08-24 13:16:26 -0500

    fix navbar reload

--- js/flipbook.js
+++ js/flipbook.js
@@ -28,31 +28,33 @@ $(document).ready(function(){
 
 	$("#flipbook").bind("turning", function(event, page, view) {
 		count = page
-		$('#number').html(count);		
-		if (page == 2) {
-			$("#flipbook").css("margin", "0 auto")
-			$(".left").css({
-				"border" : "solid rgba(0,0,0,.7)",
-				"border-width" : "0 5px 5px 0"
-			});
-		} else if (page == 1) {
-			$("#flipbook").css("margin-left", "-38vw")
-			$(".left").css({
-				"border" : "solid rgba(0,0,0,0)",
-				"border-width" : "0 5px 5px 0"
-			});
-		} else if (page == 22) {
-			$("#flipbook").css("margin-right", "-38vw");
-			$(".right").css({
-				"border" : "solid rgba(0,0,0,0)",
-				"border-width" : "0 5px 5px 0"
-			});
-		} else if (page == 21) {
-			$("#flipbook").css("margin", "0 auto")
-			$(".right").css({
-				"border" : "solid rgba(0,0,0,.7)",
-				"border-width" : "0 5px 5px 0"
-			});
+		$('#number').html(count);
+		if($(window).width() > 500) {		
+			if (page == 2) {
+				$("#flipbook").css("margin", "0 auto")
+				$(".left").css({
+					"border" : "solid rgba(0,0,0,.7)",
+					"border-width" : "0 5px 5px 0"
+				});
+			} else if (page == 1) {
+				$("#flipbook").css("margin-left", "-38vw")
+				$(".left").css({
+					"border" : "solid rgba(0,0,0,0)",
+					"border-width" : "0 5px 5px 0"
+				});
+			} else if (page == 22) {
+				$("#flipbook").css("margin-right", "-38vw");
+				$(".right").css({
+					"border" : "solid rgba(0,0,0,0)",
+					"border-width" : "0 5px 5px 0"
+				});
+			} else if (page == 21) {
+				$("#flipbook").css("margin", "0 auto")
+				$(".right").css({
+					"border" : "solid rgba(0,0,0,.7)",
+					"border-width" : "0 5px 5px 0"
+				});
+			}
 		}
 	})
 

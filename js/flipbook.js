79449c6afd0d766e030dbd4ab432c36f00bdd94c
rmaus8 <rmausolf06@gmail.com>
2018-08-24 11:33:12 -0500

    fix responsive design

--- js/flipbook.js
+++ js/flipbook.js
@@ -31,12 +31,28 @@ $(document).ready(function(){
 		$('#number').html(count);		
 		if (page == 2) {
 			$("#flipbook").css("margin", "0 auto")
+			$(".left").css({
+				"border" : "solid rgba(0,0,0,.7)",
+				"border-width" : "0 5px 5px 0"
+			});
 		} else if (page == 1) {
 			$("#flipbook").css("margin-left", "-38vw")
+			$(".left").css({
+				"border" : "solid rgba(0,0,0,0)",
+				"border-width" : "0 5px 5px 0"
+			});
 		} else if (page == 22) {
 			$("#flipbook").css("margin-right", "-38vw");
+			$(".right").css({
+				"border" : "solid rgba(0,0,0,0)",
+				"border-width" : "0 5px 5px 0"
+			});
 		} else if (page == 21) {
 			$("#flipbook").css("margin", "0 auto")
+			$(".right").css({
+				"border" : "solid rgba(0,0,0,.7)",
+				"border-width" : "0 5px 5px 0"
+			});
 		}
 	})
 
